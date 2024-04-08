64a1a5912e8d57c2821c3af7d7c785fa7c9b5927
Philipp Bieberstein <pbieberstein@imm.uzh.ch>
2024-04-09 01:08:29 +0200

    resistance_table.R script now copes with failed assemblies

--- bin/resistance_table.R
+++ bin/resistance_table.R
@@ -4,11 +4,20 @@ dat.out <- data.frame(BactNummer = samples, Resistance = NA)
 
 myres <-list()
 for(i in 1:length(samples)){
-  myres <- read.delim(file = paste0("results/",samples[i],"/4_virulence/",samples[i],"_resistance.tab"))[,c("GENE","X.COVERAGE","X.IDENTITY")]
+  # Iterate through all samples and look for resistance.tab file
+  fn <- paste0("results/",samples[i],"/4_virulence/",samples[i],"_resistance.tab")
+  # If it exists, pull out resistance genes, if not, say "Not Screened"
+  if(file.exists(fn)) {
+    myres <- read.delim(file = fn)[,c("GENE","X.COVERAGE","X.IDENTITY")]
+  } else {
+    myres <- data.frame("GENE" = "Not Screened", "X.COVERAGE"=100, "X.IDENTITY"=100)
+  }
   
   if(nrow(myres) > 0){
+    # Only include genes with coverage 100
     for(j in 1:nrow(myres)){
       myres[j,"out"] <- if(myres[j,2] == "100"){
+        # If identity is 100, just include gene name, otherwise add % identity
         if(myres[j,3] == "100"){
           as.character(myres[j,1])
         }else{
@@ -21,8 +30,5 @@ for(i in 1:length(samples)){
     dat.out[which(dat.out$BactNummer == samples[i]), "Resistance"] <- NA
   }
 }
-
-#library(DT)
-#datatable(dat.out,rownames = FALSE)
 
 write.table(x = dat.out,file = "resistances.txt",sep = "\t",row.names = FALSE, quote=FALSE)
