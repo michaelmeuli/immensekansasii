5b3829a9e9030b88a65fcabf55e9abef89fb4a43
Michael Meuli <michael.meuli@gmail.com>
2026-08-21 12:02:39 +0200

    download kansasii complex reference genomes with lit_gtdb.sh

---
+++ scripts/lit_download.sh
@@ -0,0 +1,15 @@
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/bac120_metadata_r232.tsv "$env:USERPROFILE\kansasii\downloads\"
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/bac120_taxonomy_r232.tsv "$env:USERPROFILE\kansasii\downloads\"
+
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/kansasii_complex_rows_metadata.tsv "$env:USERPROFILE\kansasii\downloads\"
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/kansasii_complex_rows_taxonomy.tsv "$env:USERPROFILE\kansasii\downloads\"
+
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/metadata_accessions.txt "$env:USERPROFILE\kansasii\downloads\"
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/taxonomy_accessions.txt "$env:USERPROFILE\kansasii\downloads\"
+
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/taxonomy_accessions.txt "$env:USERPROFILE\kansasii\downloads\"
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/type_strains_accession_biosample.tsv "$env:USERPROFILE\kansasii\downloads\"
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/type_strains_ena_reads.tsv "$env:USERPROFILE\kansasii\downloads\"
+
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/lit/gtdb/type_strains_ena_biosample_species.tsv "$env:USERPROFILE\kansasii\downloads\"
+
