acb43f79dabdcce3eda4b463bf66e01614177dcf
Michael Meuli <michael.meuli@gmail.com>
2026-09-04 16:15:22 +0200

    lit MLST

---
+++ lit/schemes.md
@@ -0,0 +1,10 @@
+| Scheme ID | Label | Description | Purpose |
+|---|---|---|---|
+| `mycobacteria_1` | Scheme 1 | Core genome (H37Rv ref vs 19 finished species) | Broad core-genome comparison across species |
+| `mycobacteria_2` | Scheme 2 (yours) | MLST | The 8-locus ribosomal typing scheme you've been using |
+| `mycobacteria_3` | Scheme 3 | MTBC cgMLST | Core-genome MLST for the *M. tuberculosis* complex |
+| `mycobacteria_4` | Scheme 4 | *M. avium* cgMLST | Core-genome MLST for *M. avium* complex strains |
+| `mycobacteria_10` | Scheme 10 | Meromycolic acid biosynthesis, FAS II | Cell wall lipid pathway genes |
+| `mycobacteria_11` | Scheme 11 | Meromycolic acid biosynthesis | Related cell wall lipid pathway genes |
+| `mycobacteria_12` | Scheme 12 | De novo purine biosynthesis pathway | Metabolic pathway genes |
+| `mycobacteria_13` | Scheme 13 | mAGP complex pathway | Mycolyl-arabinogalactan-peptidoglycan complex genes |
--- scripts/run.sh
+++ scripts/run.sh
@@ -23,6 +23,14 @@ tar -xf archive.tar
 # ssh mimeul@cluster.s3it.uzh.ch "cd /shares/sander.imm.uzh/MM/kansasii/output && tar --exclude='./*/work' -cf /tmp/archive.tar ."
 
 
+
 mkdir -p /shares/sander.imm.uzh/MM/kansasii/output/reference_genomes_gtdb_232
 cd /shares/sander.imm.uzh/MM/kansasii/output/reference_genomes_gtdb_232
 bash /shares/sander.imm.uzh/MM/kansasii/immensekansasii/run_IMMENSE.sh -j ref_232_run -t fasta -r ref_run -i /shares/sander.imm.uzh/MM/kansasii/data/reference_genomes_gtdb_232/
+
+ssh mimeul@cluster.s3it.uzh.ch "rm -f /shares/sander.imm.uzh/MM/kansasii/output/archive.tar"
+ssh mimeul@cluster.s3it.uzh.ch "cd /shares/sander.imm.uzh/MM/kansasii/output/reference_genomes_gtdb_232 && tar --exclude='work' -cf /shares/sander.imm.uzh/MM/kansasii/output/archive.tar ."
+New-Item -ItemType Directory -Path "$env:USERPROFILE\kansasii\downloads\reference_genomes_gtdb_232\" -Force
+scp mimeul@cluster.s3it.uzh.ch:/shares/sander.imm.uzh/MM/kansasii/output/archive.tar "$env:USERPROFILE\kansasii\downloads\reference_genomes_gtdb_232\"
+cd "$env:USERPROFILE\kansasii\downloads\reference_genomes_gtdb_232\"
+tar -xf archive.tar
